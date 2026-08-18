f127a670887a29bde2a9a5b36ffc65278e149961
chendaniely <chendaniely@users.noreply.github.com>
2026-08-18 00:35:37 +0000

    Remove preview for PR 28 🛬

--- pr-preview/pr-28/resources_pages/check-setup-mds_kcds-toolbox.sh
+++
@@ -1,1 +0,0 @@
-bash <(curl -Ss https://raw.githubusercontent.com/UBC-MDS/UBC-MDS.github.io/master/content/resources_pages/check-setup-mds.sh) | grep "You need \|You are recommended\|  python\|  conda\|  bash\|  git\|  code\|pandas=\|pyppeteer=\|nbconvert=\|jupyterlab=\|jupyterlab-git=\|jupyterlab-spellchecker=\|jupyter-book=\|jupyterlab WebPDF\|jupyterlab HTML" 
